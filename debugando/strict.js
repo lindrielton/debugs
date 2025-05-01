2cf6dd1d41db358d451f94138832362bce2cc3a4
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-05-01 06:56:40 -0300

    debugs

--- debugando/strict.js
+++ debugando/strict.js
@@ -1,15 +1,27 @@
-//"use strict"
 
-//user = 'true';
+let a = 1;
+let b = 2;
+let c = 5;
 
-function test(){
-    "use strict"
-    let testando = 'test';
+if(a < 2){
+    a = b + c;
+    console.log(a);
 }
 
-test();
+for(let i = 0; i < c ; i++){
+    a++;
+    a = (b + c )* 2;
+    console.log(a);
+}
 
-false.pronp = "";
-true.pronp = "um";
+if(b == a){
+    a++;
+    console.log(a);
 
+}else{
+    a = a + b + c;
+    console.log(a);
+}
 
+a = a + b + c + b;
+console.log(a);
